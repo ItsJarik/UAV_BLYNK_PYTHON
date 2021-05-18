20b3f5e519b72ff94d7ad87e15f58f9bd05d7b1b
ItsJarik <jarikokkerman@hotmail.nl>
2021-05-18 20:09:08 +0200

    Update BlynkPython.py
    - Add New Functions

--- BlynkPython.py
+++ BlynkPython.py
@@ -2,13 +2,18 @@
 
 import blynklib
 
-BLYNK_AUTH = 'tylKcaLYauctISWFL0l9KEZVK8DN77Wj'
+BLYNK_AUTH = '3cQIWyxYKPUK6j4U1u2U5NIvjyYG5g9X'
 
 # initialize Blynk
-blynk = blynklib.Blynk(BLYNK_AUTH)
+blynk = blynklib.Blynk(BLYNK_AUTH, server='192.168.2.6', port=8080)
 
 WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 
+
+# register handler for virtual pin V4 write event
+@blynk.handle_event('write V0')
+def write_virtual_pin_handler(pin, value):
+    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
 
 # register handler for virtual pin V4 write event
 @blynk.handle_event('write V4')
