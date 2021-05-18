2c5f6acadf6e6bbe245932c9a3453987654117de
ItsJarik <jarikokkerman@hotmail.nl>
2021-05-18 19:06:15 +0200

    Update BlynkPython.py
    - Add File (Main Program)

---
+++ BlynkPython.py
@@ -0,0 +1,23 @@
+# ItsJarik
+
+import blynklib
+
+BLYNK_AUTH = 'Cj_AmQrFDvZPAURuqZ6k4a7RPrwtDTCd'
+
+# initialize Blynk
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+
+
+# register handler for virtual pin V4 write event
+@blynk.handle_event('write V4')
+def write_virtual_pin_handler(pin, value):
+    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+
+
+###########################################################
+# infinite loop that waits for event
+###########################################################
+while True:
+    blynk.run()
